aaae96420d851b12ec3e09800fec7b2b19930ec3
ajisyahroni <aji.0508@students.amikom.ac.id>
2023-11-23 18:41:37 +0700

    init

---
+++ android/app/src/main/kotlin/com/akadevo/pair/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.akadevo.pair
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/modules/home/views/home_view.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+
+import '../../../../config/translations/strings_enum.dart';
+import '../../../components/api_error_widget.dart';
+import '../../../components/my_widgets_animator.dart';
+import '../controllers/home_controller.dart';
+
+class HomeView extends GetView<HomeController> {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'your home screen',
+        ),
+      ),
+    );
+  }
+}
